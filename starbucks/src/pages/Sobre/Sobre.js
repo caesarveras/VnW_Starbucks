8c31ee1a349705062fe9aaa3f05a11af78c151cd
caesarveras <caesarveras@letras.ufrj.br>
2024-12-03 10:19:41 -0300

    release5

--- starbucks/src/pages/Sobre/Sobre.js
+++ starbucks/src/pages/Sobre/Sobre.js
@@ -6,15 +6,17 @@ const Sobre = () => {
     <Container>
      
       <Content>
-        <Image src="/images/starbucks-store.jpg" alt="Starbucks Store" />
+       
+        <Image src="https://raw.githubusercontent.com/caesarveras/VnW_Starbucks/3280c3e3aa261ce79cf0d556f0772ab23e8dccd8/starbucks/public/images/starbucks_antigo.svg" alt="Starbucks Store" />
         <InfoSection>
-          <h2>Preparação</h2>
-          <h3>Níveis de torra</h3>
-          <p>
-            Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas são as torras
-            que fazem parte dos níveis de torra Starbucks®.
-          </p>
-          <Button>Saiba Mais</Button>
+
+          <h2>PREPARAÇÃO</h2>
+        <h3>Níveis de torra</h3>
+        <p>
+        Qual a torra que prefere? Starbucks® Torra Clara, Torra Média ou Torra Escura? Estas sãos as torras que fazem parte dos níveis de torra Starbucks®.
+        </p>
+        <button className="novidades-button"><b>SAIBA MAIS</b></button>
+
         </InfoSection>
       </Content>
     </Container>
--- starbucks/src/pages/Sobre/SobreStyles.js
+++ starbucks/src/pages/Sobre/SobreStyles.js
@@ -1,8 +1,9 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  font-family: 'Arial', sans-serif;
-  background-color: #1e3932;
+margin: 100px;
+  font-family: 'Poppins', sans-serif;
+  background-color: #fff;
   color: #333;
   padding: 20px;
 `;
@@ -10,6 +11,7 @@ export const Container = styled.div`
 export const Header = styled.header`
   display: flex;
   justify-content: space-between;
+  font-family: 'Poppins', sans-serif;
   align-items: center;
   padding: 10px 20px;
   background-color: #fff;
@@ -17,6 +19,7 @@ export const Header = styled.header`
   nav a {
     margin: 0 15px;
     text-decoration: none;
+    font-family: 'Poppins', sans-serif;
     color: #006241;
     font-weight: bold;
   }
@@ -25,46 +28,55 @@ export const Header = styled.header`
 export const Content = styled.div`
   display: flex;
   justify-content: space-between;
+  font-family: 'Poppins', sans-serif;
   margin-top: 20px;
 `;
 
 export const Image = styled.img`
   max-width: 50%;
   height: auto;
-  border-radius: 8px;
+  border-radius: 0px;
 `;
 
 export const InfoSection = styled.div`
   max-width: 45%;
   padding: 20px;
-  background-color: #1E3932;
+  background-color: #fff;
   border-radius: 8px;
+  font-family: 'Poppins', sans-serif;
   //box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
   
   h2 {
-    font-size: 1.2em;
-    color: #ffffff;
+    font-size: 1.0em;
+    color: #1E3932;
+    font-family: 'Poppins', sans-serif;
+    
   }
 
   h3 {
     font-size: 1.8em;
-    color: #ffffff;
+    color: #1E3932;
+    font-family: 'Poppins', sans-serif;
     margin-top: 10px;
+    font-weight: normal;
   }
 
   p {
     font-size: 1em;
-    color: #ffffff;
+    color: #1E3932;
+    font-family: 'Poppins', sans-serif;
     margin: 10px 0;
   }
 `;
 
 export const Button = styled.button`
+  margin: 10px;
   padding: 10px 20px;
   background-color: #006241;
   color: #fff;
   font-size: 1em;
   border: none;
+  font-family: 'Poppins', sans-serif;
   border-radius: 40px;
   cursor: pointer;
   
